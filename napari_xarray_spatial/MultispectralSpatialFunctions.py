29d5771e774298610f48e699fe72c13901c586c1
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-06-25 06:59:36 +1000

    Add some explanatory comments

--- napari_xarray_spatial/MultispectralSpatialFunctions.py
+++ napari_xarray_spatial/MultispectralSpatialFunctions.py
@@ -30,28 +30,60 @@ AVAILABLE_FUNCS = {
 }
 
 class MultispectralFunctions(QWidget):
+    """This widget handles setting up and populating the GUI layout with
+    the available functions, and their parameters once a function is selected.
+    """
     def __init__(self, napari_viewer):
+        """Initializes the widget with the required layouts, buttons and selection
+        tools
+
+        # this QWidget.__init__ requests the napari viewer instance. 
+        # This can be done in one of two ways:
+        # 1. use a parameter called `napari_viewer`, as done here
+        # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+        """
         super().__init__()
         self.viewer = napari_viewer
 
+        # dropdown will hold all the available functions
         self.func_dropdown = QComboBox(self)
         self.func_dropdown.addItems(list(AVAILABLE_FUNCS.keys()))
+        # when the index changes, the selectionchange callback handles
+        # showing the correct parameters
         self.func_dropdown.currentIndexChanged.connect(self.selectionchange)
 
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.func_dropdown)
 
+        # the params widget will hold all the parameters for the chosen function
         self.params = QWidget()
-        self.run_btn = QPushButton("Compute Layer", self)
-
         self.params.setLayout(QVBoxLayout())
+
+        self.run_btn = QPushButton("Compute Layer", self)
         self.params.layout().addWidget(self.run_btn)
 
         self.layout().addWidget(self.params)
         self.selectionchange(0)
         
     def selectionchange(self, i):
+        """Inspects parameters of function currently selected and 
+        adds a dropdown for each one, with all napari layers as options
+
+        Parameters
+        ----------
+        i : int
+            Index currently selected
+
+        Raises
+        ------
+        TypeError
+            if the chosen function takes no xarray.DataArray parameters
+        """
+        # empty current selection
         self.clear_layout(self.params.layout())
+        
+        # inspect the function for its parameters, and only keep ones annotated
+        # with xarray.DataArray
         current_func = AVAILABLE_FUNCS[self.func_dropdown.currentText()]
         current_func_params = inspect.signature(current_func).parameters
         layer_params = []
@@ -60,6 +92,7 @@ class MultispectralFunctions(QWidget):
                 param.annotation is xarray.core.dataarray.DataArray:
                 layer_params.append(param_name)
 
+        # if we have any xarray parameters, build a new combo box for them
         if layer_params:
             combos = {}
             for layer_name in layer_params:
@@ -67,6 +100,8 @@ class MultispectralFunctions(QWidget):
                 combos[layer_name] = new_combo
 
             run_btn = QPushButton('Compute Layer', self)
+            # we curry the callback function with the combo box, current function and viewer, so that
+            # we can see what the user has chosen when they click "Compute Layer"
             run_func = compute_func_on_layers(layer_selection_combo=combos, func=current_func, viewer=self.viewer)
             run_btn.clicked.connect(run_func)
             self.params.layout().addWidget(run_btn)
@@ -75,26 +110,39 @@ class MultispectralFunctions(QWidget):
         else:
             raise TypeError("Function {current_func} does not take any xarray.DataArray parameters.")
 
-    def on_click(self, e):
-        print(f"Clicked and current selection is {self.func_dropdown.currentText()}")
-        new_label = QLabel(self)
-        new_label.setText(self.func_dropdown.currentText())
-        self.params.layout().addWidget(new_label)
-
-
     def clear_layout(self, layout):
+        """Removes all widgets from the given layout
+
+        Parameters
+        ----------
+        layout : QLayout
+            Layout containing 0 or more widgets for deletion
+        """
         while layout.count():
             child = layout.takeAt(0)
             if child.widget():
                 child.widget().deleteLater()
 
     
-    def add_layer_selection(self, layer_name):
+    def add_layer_selection(self, combo_label):
+        """Adds combo box with all napari layers, labelled with given
+        combo_label
+
+        Parameters
+        ----------
+        combo_label : str
+            label for this new combo box
+
+        Returns
+        -------
+        QComboBox
+            newly created combo box
+        """
         layer_row = QWidget()
         layer_row.setLayout(QHBoxLayout())
         
         layer_label = QLabel(self)
-        layer_label.setText(layer_name)
+        layer_label.setText(combo_label)
 
         layer_combo = QComboBox(self)
         layer_combo.addItems([layer.name for layer in self.viewer.layers])
@@ -107,10 +155,29 @@ class MultispectralFunctions(QWidget):
 
 @tz.curry
 def compute_func_on_layers(e, *args, layer_selection_combo, func, viewer):
-    print(f"Computing {func}!")
+    """Compute given func on layers selected in layer_selection_combo and 
+    add return value to viewer
+
+    Parameters
+    ----------
+    e : event
+        the button click event (given by Qt)
+    layer_selection_combo : Dict[str, QComboBox]
+        dictionary of argument name to QComboBox for selecting layers
+    func : function
+        function to run on the selected layers
+    viewer : napari Viewer
+        the napari viewer to add image to
+
+    Raises
+    ------
+    ValueError
+        if any of the layer data is not 2D
+    """
     x_arr_args = {}
     for arg_name, combo in layer_selection_combo.items():
         layer_name = combo.currentText()
+        # each layer is a layer object - its underlying data (whether it be numpy, dask, etc.) is always at layer.data
         given_layer_data = viewer.layers[layer_name].data
         if len(given_layer_data.shape) != 2:
             raise ValueError(f"{func} can only be applied to 2D data, but {layer_name} is of shape {given_layer_data.shape}")
@@ -122,5 +189,5 @@ def compute_func_on_layers(e, *args, layer_selection_combo, func, viewer):
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    # you can return either a single widget, or a sequence of widgets
+    # this is where napari looks to find your widget(s). You can return a list of widgets if desired.
     return MultispectralFunctions
